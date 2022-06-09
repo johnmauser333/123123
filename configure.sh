0e24c566338c67289486bd974ea9db4bcc6bd0ac
johnmauser333 <86488300+johnmauser333@users.noreply.github.com>
2022-06-09 22:12:47 +1000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 
 # Download and install V2Ray
 mkdir /tmp/xray
-wget -q https://github.com/XTLS/Xray-core/releases/download/v1.5.4/Xray-linux-64.zip -O /tmp/xray/xray.zip
+wget -q https://github.com/XTLS/Xray-core/releases/download/v1.5.5/Xray-linux-64.zip -O /tmp/xray/xray.zip
 unzip /tmp/xray/xray.zip -d /tmp/xray
 install -m 755 /tmp/xray/xray /usr/local/bin/xray
 
@@ -15,59 +15,28 @@ cat << EOF > /usr/local/etc/xray/config.json
 {
     "inbounds": [
         {
-            "tag": "in_tomcat",
-            "port": $PORT,
-            "protocol": "dokodemo-door",
-            "settings": {
-                "address": "127.0.0.1",
-                "port": 8020,
-                "network": "tcp"
-            }
-        },
-        {
-            "tag": "in_interconn",
             "port": $PORT,
             "protocol": "vmess",
             "settings": {
                 "clients": [
                     {
                         "id": "$UUID",
-                        "alterId": 0,
-                        "security": "chacha20-poly1305"
+                        "security": "chacha20-poly1305",
+                        "alterId": 0
                     }
-                ]
+                ],
+                "disableInsecureEncryption": true
             },
             "streamSettings": {
-              "network": "ws"
+                "network": "ws"
             }
         }
     ],
-    "reverse": {
-        "portals": [
-            {
-                "tag": "portal",
-                "domain": "yoshimitsu737-freenom.ml"
-            }
-        ]
-    },
-    "routing": {
-        "rules": [
-            {
-                "type": "field",
-                "inboundTag": [
-                    "in_tomcat"
-                ],
-                "outboundTag": "portal"
-            },
-            {
-                "type": "field",
-                "inboundTag": [
-                    "in_interconn"
-                ],
-                "outboundTag": "portal"
-            }
-        ]
-    }
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
 }
 EOF
 
